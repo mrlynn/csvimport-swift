13b5d73c97f5c5ebd10131b1f4050141199d5e05
Michael Lynn <merlynn@gmail.com>
2020-08-28 12:51:35 -0400

    update to use mongo-swift-driver and add example.csv

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # csvimport-swift
 Import CSV File using MongoDB Swift Driver
+
+## As seen on Twitch.tv/mongodb August 28th, 2020
--- Sources/csvimport-swift/main.swift
+++ Sources/csvimport-swift/main.swift
@@ -1,1 +1,48 @@
-print("Hello, world!")
+import Foundation
+import MongoSwiftSync
+
+var murl: String = "mongodb://localhost:27017/students"
+let client = try MongoClient(murl)
+
+let db = client.db("students")
+let session = client.startSession(options: ClientSessionOptions(causalConsistency: true))
+
+struct Person: Codable {
+    let firstname: String
+    let lastname: String
+    let date: Date = Date()
+    let assigned: Bool
+    let _id: BSONObjectID
+}
+
+let path = "/Users/mlynn/Desktop/example.csv"
+let personCollection = db.collection("people", withType: Person.self)
+
+var tempAssigned: Bool
+var count: Int = 0
+
+do {
+    let contents = try String(contentsOfFile: path, encoding: .utf8)
+    let rows = contents.components(separatedBy: NSCharacterSet.newlines)
+    var header: Bool = true
+    for row in rows {
+        if row != "" {
+            var values: [String] = []
+            values = row.components(separatedBy: ",")
+            if header == true {
+                header = false
+            } else {
+                if String(values[2]).lowercased() == "false" || Bool(values[2]) == false {
+                    tempAssigned = false
+                } else {
+                    tempAssigned = true
+                }
+                try personCollection.insertOne(Person(firstname: values[0], lastname: values[1], assigned: tempAssigned, _id: BSONObjectID()), session: session)
+                count.self += 1
+                print("Inserted: \(count) \(row)")
+                
+            }
+        }
+    }
+}
+
